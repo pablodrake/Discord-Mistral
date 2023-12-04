85b4b31c46cb0564088516020d79c56bdd2a3396
pdaloxd <31321612+pablodrake@users.noreply.github.com>
2023-12-04 23:22:56 +0100

    Create responses.py
    handle responses

---
+++ responses.py
@@ -0,0 +1,18 @@
+import concurrent.futures
+from langchain.llms import Ollama
+
+
+mistral = Ollama(base_url='http://localhost:11434', model='mistral')
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    words = p_message.split()
+    if words[0] == "!mistral":
+        prompt = " ".join(words[1:])
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            future = executor.submit(mistral, promt)
+            result = future.result()
+            return result
+    else:
+        return 'If you want to talk to the AI write a message that starts with "!mistral" followed by a blank space'
